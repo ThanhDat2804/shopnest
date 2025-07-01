ae6025523589240afb6aece5e6729f212ec7e655
ThanhDat2804 <dat.le@savvycom.vn>
2025-07-01 14:16:29 +0700

    fix cant define

--- mall-security/src/main/java/com/mall/shopnest/security/component/DynamicSecurityService.java
+++ mall-security/src/main/java/com/mall/shopnest/security/component/DynamicSecurityService.java
@@ -2,9 +2,11 @@ package com.mall.shopnest.security.component;
 
 
 import org.springframework.security.access.ConfigAttribute;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+@Service
 public interface DynamicSecurityService {
 
     Map<String, ConfigAttribute> loadDataSource();
